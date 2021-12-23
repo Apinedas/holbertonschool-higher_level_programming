76c8a650331442fbd8e6cfdcd584c40453655e01
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-12-23 21:58:47 +0000

    Added 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+
+    sum = 0
+    i = 1
+    argc = len(sys.argv)
+
+    while (i < argc):
+        sum += int(sys.argv[i])
+        i += 1
+    
+    print("{:d}".format(sum))
