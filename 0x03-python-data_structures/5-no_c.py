2412d8896fae3bda0bdd0ac6909f57ad4c7cf6ba
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-01-03 23:22:13 +0000

    Correction on 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@ def no_c(my_string):
     new_string = ''
     for char in my_string:
         if char != 'c' and char != 'C':
-            new_string = new_string  + char
+            new_string = new_string + char
         i += 1
     return new_string
