b2485f9179540df16592e5a7c03f37ccf4636a57
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-01-04 02:06:44 +0000

    Added 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,58 +1,39 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the head of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
-}
-
-/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to pointer of first node of listint_t list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *reversed, *current, *current_rev;
+    listint_t *current, *compare;
+	int list_lenght = 0, movements = 0, moved = 0;
 
-	current = *head;
-	reversed = NULL;
-	while (current)
-	{
-		add_nodeint(&reversed, current->n);
-		current = current->next;
-	}
-	current = *head;
-	current_rev = reversed;
-	while (current && current_rev)
-	{
-		if (current->n == (current_rev)->n)
-		{
-			current = current->next;
-			current_rev = (current_rev)->next;
-		}
-		else
-		{
-			free_listint(reversed);
-			return (0);
-		}
-	}
-	free_listint(reversed);
-	return (1);
+    current = *head;
+    while (current)
+    {
+        list_lenght++;
+        current = current->next;
+    }
+    movements = list_lenght - 1;
+    current = *head;
+    while (movements > 0)
+    {
+        compare = current;
+        while(moved != movements)
+        {
+            compare = compare->next;
+            moved++;
+        }
+        if (current->n == compare->n)
+        {
+            current = current->next;
+            movements -= 2;
+            moved = 0;
+        }
+        else
+            return (0);
+    }
+    return (1);
 }
