8bcd2a8d0557708b70a9d545b4417483872a0ec5
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-01-24 23:29:46 +0000

    Added mandatory

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""Module for Rectangle class
+"""
+
+
+from cmath import rect
+
+
+class Rectangle:
+    """This class describes a rectangle
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """ Rectangle's width (int) """
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Rectangle's height (int) """
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    """ Instance method to calc Rectangle's area """
+    def area(self):
+        return (self.width * self.height)
+
+    """ Instance method to calc Rectangle's perimeter """
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return (0)
+        return ((2 * self.width) + (2 * self.height))
+
+    """ __str__ method to print the Rectangle """
+    def __str__(self):
+        ret = ""
+        if self.height == 0 or self.width == 0:
+            return (ret)
+        for i in range(self.height):
+            for j in range(self.width):
+                ret += str(self.print_symbol)
+            ret += "\n"
+        ret = ret[0:-1]
+        return (ret)
+
+    """ __repr__ method to recreate the Rectangle """
+    def __repr__(self):
+        return ("Rectangle(" + str(self.width) + ", " + str(self.height) + ")")
+
+    """ __del__ method to print a message when
+    an instance is deleted """
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """ Static method which compares two rectangles """
+        if isinstance(rect_1, Rectangle) is False:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if isinstance(rect_2, Rectangle) is False:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return (rect_2)
