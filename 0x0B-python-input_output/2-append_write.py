6f06be52ac4786feb5fe5be001cd8b6237c554bf
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-02-02 04:27:53 +0000

    Added mandatory

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Module with a writing function
+"""
+
+
+def write_file(filename="", text=""):
+    """
+        write_file - writes some text on a file. This doesn't overwrite the
+        content of the file.
+    """
+    with open(filename, encoding='utf-8', mode='a') as file:
+        file.write(text)
