7a4b0eefe0eca86c7096bb9d53b366332d518bf7
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-02-09 02:48:56 +0000

    Updated tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -25,6 +25,36 @@ class TestBase(unittest.TestCase):
     def test_base_id(self):
         self.assertEqual(self.base_3.id, 5)
 
+    def test_base_tojsonstringnone(self):
+        ret = "[]"
+        test_list = self.base_1.to_json_string(None)
+        self.assertEqual(ret, test_list)
+
+    def test_base_tojsonstringempty(self):
+        ret = "[]"
+        test_list = self.base_1.to_json_string([])
+        self.assertEqual(ret, test_list)
+
+    def test_base_tojsonstringdict(self):
+        test_dict = [{'id': 7}]
+        ret = "[{\"id\": 7}]"
+        self.assertEqual(self.base_1.to_json_string(test_dict), ret)
+
+    def test_base_fromjsonstringnone(self):
+        ret = []
+        test_list = self.base_1.from_json_string(None)
+        self.assertEqual(ret, test_list)
+
+    def test_base_fromjsonstringempty(self):
+        ret = []
+        test_list = self.base_1.from_json_string([])
+        self.assertEqual(ret, test_list)
+
+    def test_base_fromjsonstringdict(self):
+        ret = [{'id': 7}]
+        test_dict = "[{\"id\": 7}]"
+        self.assertEqual(self.base_1.from_json_string(test_dict), ret)
+
 
 if __name__ == '__main__':
     unittest.main()
