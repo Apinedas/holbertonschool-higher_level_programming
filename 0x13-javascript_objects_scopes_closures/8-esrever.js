1d574f26c25ba1f03137236f41d97887cc06c90c
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-04-07 11:42:49 -0500

    fix: change on 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   let len;
-  let copy;
-  let aux;
+  let copy = [];
+  let aux = 0;
   for (len = 0; list[len]; len++) {
     continue;
   }
-  if (len <= 1) {
-    return (list);
+  for (len -= 1; len >= 0; len--, aux++) {
+    copy[aux] = list[len];
   }
-  for (copy = 0; copy < len; copy++, len--) {
-    aux = list[copy];
-    list[copy] = list[len - 1];
-    list[len] = aux;
-  }
-  return (list);
+  return (copy);
 };
