9a951a6aa51c0393a40bfacebd0298f995dc8697
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-04-07 17:51:57 -0500

    fix: Semistandard correction on 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,13 +6,13 @@ const entriesList = Object.entries(dictFrom);
 const dictTo = {};
 valuesList.sort();
 valuesList.forEach(element => {
-    if (!(element in dictTo)){
-        dictTo[element] = [];
-    }
-})
+  if (!(element in dictTo)) {
+    dictTo[element] = [];
+  }
+});
 entriesList.forEach(entrie => {
-    if (entrie[1] in dictTo) {
-        dictTo[`${entrie[1]}`].push(entrie[0]);
-    }
+  if (entrie[1] in dictTo) {
+    dictTo[`${entrie[1]}`].push(entrie[0]);
+  }
 });
 console.log(dictTo);
