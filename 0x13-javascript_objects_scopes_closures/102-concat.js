d3d2e5564e0c62393bed393bf37b8ca58e5f2c9f
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-04-07 18:17:15 -0500

    feat: Added 102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const fs = require('fs');
+
+fs.readFile(argv[2], (err, data) => {
+  if (!err) {
+    fs.writeFile(argv[4], data, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
+
+fs.readFile(argv[3], (err, data) => {
+  if (!err) {
+    fs.appendFile(argv[4], '\n', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    fs.appendFile(argv[4], data, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
