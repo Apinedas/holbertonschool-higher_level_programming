69eb85504bc078b71b274062009d960d39e00b46
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-04-07 11:24:17 -0500

    fix: changed 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,34 +1,10 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Rectangle = require('./4-rectangle.js');
 
-  print () {
-    let i;
-    let j;
-    let line = '';
-    for (i = 0; i < this.width; i++) {
-      line += 'X';
-    }
-    for (j = 0; j < this.height; j++) {
-      console.log(line);
-    }
-  }
-
-  rotate () {
-    const aux = this.width;
-    this.width = this.height;
-    this.height = aux;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
 
   charPrint (c) {
@@ -43,11 +19,5 @@ class Rectangle {
     for (i = 0; i < this.width; i++) {
       console.log(line);
     }
-  }
-}
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
   }
 };
