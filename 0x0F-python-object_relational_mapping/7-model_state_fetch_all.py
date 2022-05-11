e5da45725b774da5304ee9b0352c0e2d69289cf9
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-11 15:22:54 -0500

    fix: Changed 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
 
     metadata = MetaData()
 
-    Base.metadata.create_all(engine)
     session = Session(engine)
 
     for state in session.query(State).order_by(State.id).all():
