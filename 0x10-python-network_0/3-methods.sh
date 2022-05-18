23c0396355928a6a77ee86ea26d901bc42bb23fa
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-18 16:01:20 -0500

    feat: Added 4

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# prints allowed HTTP methods
 curl -sI {$1} | grep Allow | cut -d" " -f2-
---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sets a header variable and send GET
+curl -sH "X-HolbertonSchool-User-Id: 98" {$1}
