caa1cbf655d51a60bfdd2ec4457abd630119d5fc
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-18 16:43:22 -0500

    fix: Style and doc

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 '''Module to find a peak'''
+
+
 def find_peak(list_of_integers):
+    '''Peak function for a integers list'''
     if list_of_integers:
         sorted_list = sorted(list_of_integers)
         return sorted_list[-1]
