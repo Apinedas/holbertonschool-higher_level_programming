658411633531a703d6b3caca53070606eb72f9f5
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-19 16:25:25 -0500

    feat: added 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     email = {"email": argv[2]}
     encoded_data = urlencode(email).encode("utf-8")
-    req = urllib.request.Request(argv[1], data=encoded_data)
+    req = urllib.request.Request(argv[1], encoded_data)
     with urllib.request.urlopen(req) as response:
         page = response.read()
         print(page.decode("utf-8"))
