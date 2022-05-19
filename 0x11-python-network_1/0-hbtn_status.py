04a5ff8ca95a0261cd3b15485aee76c2dfc7c845
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-19 15:39:32 -0500

    feat: Added 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 '''script that fetches https://intranet.hbtn.io/status'''
 
-from encodings import utf_8, utf_8_sig
-import urllib.request
 
-req = urllib.request.Request('https://intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    page = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(page)))
-    print("\t- content: {}".format(page))
-    print("\t- utf8 content: {}".format(page.decode("utf8")))
+if __name__ == "__main__":
+    import urllib.request
+
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode("utf8")))
