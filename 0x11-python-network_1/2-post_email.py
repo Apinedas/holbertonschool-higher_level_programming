217de47a677815025b439921681e36410ad978f7
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-19 16:19:29 -0500

    feat: added 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''script that POST email to argv[1]'''
+
+
+if __name__ == "__main__":
+    import urllib.request
+    from urllib.parse import urlencode
+    from sys import argv
+
+    email = {"email": argv[2]}
+    encoded_data = urlencode(email).encode("utf8")
+    req = urllib.request.Request(argv[1], data=encoded_data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print(page.decode('utf8'))
