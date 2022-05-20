5198fd0943bfbfa7dcf46d3cff9df559778f9f6c
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-20 10:54:34 -0500

    feat: Added 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''fetches https://intranet.hbtn.io/status and print X-Request-Id'''
 
+if __name__ == "__main__":
+    import requests
+    import sys
 
-import requests
-import sys
-
-r = requests.get(sys.argv[1])
-print(r.headers.get('X-Request-Id'))
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
